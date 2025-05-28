a835f1bb7c6af5cf31743e0f8a6f51685c48a1e9
AbdelrahmanW08 <abdelrahman.wm2@gmail.com>
2025-05-28 18:21:52 -0400

    set up basic frontend and backend

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+export const signup = (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    res.send("signup");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const login = (req, res) => {
+  res.send("login");
+};
+
+export const logout = (req, res) => {
+  res.send("logout");
+};
